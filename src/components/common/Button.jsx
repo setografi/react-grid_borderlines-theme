6f6b5028ee54f369d6fc783be5ca7c029df278b1
setografi <hitonaashbell@gmail.com>
2024-12-26 10:36:54 +0700

    update: Button

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Button() {
-  return <div>Button</div>;
+  return (
+    <button
+      className="group w-fit py-1 px-3 border border-lightGray-700 rounded-lg 
+  bg-lightGray-500 hover:bg-lightGray-600 focus:ring-2 focus:ring-lightGray-700 focus:outline-none active:bg-lightGray-700 transition duration-300"
+    >
+      <span className="text-sm text-darkGray-500 group-hover:text-darkGray-400 group-active:text-darkGray-300">
+        <i className="ri-arrow-right-s-line"></i>
+      </span>
+    </button>
+  );
 }
 
 export default Button;
